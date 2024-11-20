6e00c37bf9795fa9fde5df8145b0235db7e9d886
MilesG11 <milesgrant132@gmail.com>
2024-11-20 12:40:06 -0800

    Added simple topnav bar to move between pages

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ var report_list; // Report list HTML element
 var markers = []; // Markers array
 
 function initializeHome() {
+    // Add top nav bar
+    const topnav = document.createElement("div");
+    topnav.className = "topnav";
+    topnav.innerHTML = `
+        <a class="active" href="index.html">Home</a>
+        <a href="report.html">Create Report</a>
+        `
+    document.body.appendChild(topnav);
+    
     // Dynamically initialize map
     let map_container = document.createElement('div');
     map_container.id = 'map';
